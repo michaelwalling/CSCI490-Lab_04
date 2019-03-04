c71bb533c194a7facb6671d679354c6834ce922f
Michael Walling <wallingm@g.cofc.edu>
2019-03-04 18:39:57 -0500

    webview lab

--- app/src/main/java/com/example/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/csci490_lab_04/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+
+        webview.loadUrl("https://www.google.com/");
+
+        //makeURLConnection("URL");
+
+
     }
 }
